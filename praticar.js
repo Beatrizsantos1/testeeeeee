39e077bd0519ae8f9f74c7c2a5fe4dc8f705c0af
Beatrizsantos1 <beatrizoliveiramr@gmail.com>
2024-04-23 16:18:48 -0300

    criacao do botao da pagina praticar

---
+++ praticar.js
@@ -0,0 +1,9 @@
+let btnScratch = document.querySelector(".btn-slide1");
+
+function redirecionamenScratch() {
+  window.open("https://scratch.mit.edu/download", "_blank");
+}
+
+btnScratch.addEventListener("click", function () {
+  redirecionamenScratch();
+});
