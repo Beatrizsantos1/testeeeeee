d584a1de86fd448bb9d1847748da72a07e7a7f10
Beatrizsantos1 <beatrizoliveiramr@gmail.com>
2024-04-25 23:36:38 -0300

    criação do JS na parte da teoria

--- teoria.js
+++ teoria.js
@@ -20,3 +20,18 @@ fecharTela.addEventListener("click", function () {
   fechar();
 });
 /* ------------------fim do botão voltar a tela anterior --------------------*/
+
+/* ------------------Inicio do botão para segunda pagina teoria --------------------*/
+const btnsegundaPagina = document.getElementById("segundaPagina");
+
+function proximaPagina() {
+  window.location.href = "teoria2.html";
+}
+
+btnsegundaPagina.addEventListener("click", function () {
+  proximaPagina();
+});
+/* ------------------Fim do botão para segunda pagina teoria --------------------*/
+function telaseguinte() {
+  window.location = "teoria3.html";
+}
